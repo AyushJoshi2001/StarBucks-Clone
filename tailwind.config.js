d48d987060a01dc369d07f7c8c26a53190cb3b94
Ayush Joshi <59890645+AyushJoshi2001@users.noreply.github.com>
2021-06-20 13:57:41 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  
+  theme: {
+    extend: {backgroundImage: theme => ({
+      'desktop-background': "url('img/star-background.webp')",
+      'mobile-background': "url('https://www.starbucks.com/weblx/images/rewards/hero/hero-mobile_2021.jpg')",
+      }),
+      padding: {
+        '4/5':'80%',
+        '6.66':'15%' 
+      }    
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
